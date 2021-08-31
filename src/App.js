f6ff50152820523a0b22960e0243022986ee3aff
Fatoumata86 <soukounafatoumata86@gmail.com>
2021-08-31 20:21:46 +0200

    first part front-page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import Grille from './components/Grille'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/main.scss';
 
 class Acceuil extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { handleStartGame: false }
 
-  handleStartGame = () => {
-   
   }
+  handleStartGame = () => {
+    this.setState({ handleStartGame: true })
+  }
+  render() {
+    return (
+      <>
 
-  render() { 
-   return (  
-    <>
-    <h1>Puissance 4</h1>
-    <button className="button" type="button" onClick={this.handleStartGame()}>Démarrer le jeu</button>
-    </>
+        <h1>Puissance 4</h1>
+        <button className="button" type="button" onClick={this.handleStartGame}>Démarrer le jeu</button>
+        {this.state.handleStartGame &&
+        <Grille />
+        }
+
+      </>
     );
   }
 }
- 
+
 export default Acceuil;
