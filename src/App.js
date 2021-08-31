47865a588185eb38830e8b2ec8ae9120cd05a3a3
Fatoumata86 <soukounafatoumata86@gmail.com>
2021-08-31 16:20:38 +0200

    front-page en cours

--- src/App.js
+++ src/App.js
@@ -1,43 +1,21 @@
 import React from 'react';
-import Case from './components/Case';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/main.scss';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {  }
+class Acceuil extends React.Component {
+
+  handleStartGame = () => {
+   
   }
-  renderGrill = () => {
-   let grille = []
-    for (let casier = 1; casier <= 42; casier++) {
-      
-       grille.push(<Case />)
-    }
-        return grille
-  }
- 
+
   render() { 
-   
-
-   
-
-    return (  
-      <>
-        <h1>Puissance 4</h1>
-        <div className="containers">
-
-          
-
-            {this.renderGrill()}
-            
-        
-        </div>
-
-
-      </>
+   return (  
+    <>
+    <h1>Puissance 4</h1>
+    <button className="button" type="button" onClick={this.handleStartGame()}>Démarrer le jeu</button>
+    </>
     );
   }
 }
  
-export default App;
+export default Acceuil;
