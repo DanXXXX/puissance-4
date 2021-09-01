7d62419809be26b8514d02614894d4bfb837a634
Fatoumata86 <soukounafatoumata86@gmail.com>
2021-09-01 11:52:54 +0200

    finish front-page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import Grille from './components/Grille'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './styles/main.scss';
+import React from "react";
+import Grille from "./components/Grille";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/main.scss";
 
 class Acceuil extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = { handleStartGame: false }
+    super(props);
+    this.state = {
+      handleStartGame: false,
+    };
+    console.log(this.state.board);
+  }
+  onStartGame = () => {
+    this.setState({ handleStartGame: true });
+  };
 
-  }
-  handleStartGame = () => {
-    this.setState({ handleStartGame: true })
-  }
   render() {
     return (
       <>
-
         <h1>Puissance 4</h1>
-        <button className="button" type="button" onClick={this.handleStartGame}>Démarrer le jeu</button>
-        {this.state.handleStartGame &&
-        <Grille />
-        }
-
+        <button className="button" type="button" onClick={this.onStartGame}>
+          Démarrer le jeu
+        </button>
+        {this.state.handleStartGame && <Grille />}
       </>
     );
   }
--- src/components/Grille.jsx
+++ src/components/Grille.jsx
@@ -1,27 +1,18 @@
-import React from 'react';
-import Case from './Case';
+import React from "react";
+import Case from "./Case";
 
 class Grille extends React.Component {
-
-renderGrill = () => {
-   let grille = []
+  renderGrill = () => {
+    let grille = [];
     for (let casier = 1; casier <= 42; casier++) {
-      
-       grille.push(<Case />)
+      grille.push(<Case />);
     }
-        return grille
+    return grille;
+  };
+
+  render() {
+    return <div className="containers">{this.renderGrill()}</div>;
   }
-
-    render() { 
-        return ( 
-
-            <div className="containers">
-
-                {this.renderGrill()}
-
-            </div>
-         );
-    }
 }
- 
+
 export default Grille;
