e339c51af391deb4e5ed6a5cb09a559b4fa67579
Fatoumata86 <soukounafatoumata86@gmail.com>
2021-09-02 18:00:14 +0200

    changecolor complete

--- src/App.js
+++ src/App.js
@@ -8,33 +8,23 @@ class Acceuil extends React.Component {
     super(props);
     this.state = {
       handleStartGame: false,
-      player: 1,
-      index: 0,
-      toFill: [],
     };
-    console.log(this.state.board);
   }
+
   onStartGame = () => {
     this.setState({ handleStartGame: true });
   };
-  handleInput(e) {
-    const toFill = this.state.toFill;
-    toFill.push(+e.target.id);
-    console.log(toFill);
-    // this.setState({ toFill: push(e.target.id + 7 * 5) });
-  }
+
   render() {
     return (
       <>
         <h1>Puissance 4</h1>
-        <button className="button" type="button" onClick={this.onStartGame}>
-          Démarrer le jeu
-        </button>
-        {this.state.handleStartGame && (
-          <Grille
-            toFill={this.state.toFill}
-            onClick={this.handleInput.bind(this)}
-          />
+        {this.state.handleStartGame ? (
+          <Grille />
+        ) : (
+          <button className="button" type="button" onClick={this.onStartGame}>
+            Démarrer le jeu
+          </button>
         )}
       </>
     );
--- src/components/Case.jsx
+++ src/components/Case.jsx
@@ -3,18 +3,7 @@ import React from "react";
 class Case extends React.Component {
   render() {
     return (
-      <div
-        style={
-          {
-            //   backgroundColor: `${
-            //     this.props.toFill.some(+this.props.id) ? "red" : "white"
-            //   }`,
-          }
-        }
-        className="case"
-        id={this.props.id}
-        onClick={this.props.onClick}
-      ></div>
+      <div className="case" style={{ backgroundColor: this.props.color }}></div>
     );
   }
 }
